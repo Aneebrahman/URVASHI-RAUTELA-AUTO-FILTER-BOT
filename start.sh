a39ff3993898b0cf647acd3413c7e44ecf46dff6
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 13:58:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/Elza.git /Azanpopz
+  git clone https://github.com/Azanpopz/Elsa.git /Azanpopz
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Azanpopz
