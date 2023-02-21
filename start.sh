4637820ddda8f2f8e547f334f8a91cdb82256745
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 13:57:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/Elsa.git /Elsa
+  git clone https://github.com/Azanpopz/Elza.git /Azanpopz
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /Azanpopz
 fi
-cd /Elsa
+cd /Azanpopz
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "Starting Azanpopz...."
 python3 bot.py
